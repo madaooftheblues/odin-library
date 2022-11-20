d4bbadc6251c9eeeadd71e1e0489ac50e2c18094
madaooftheblues <azlanrasool@gmail.com>
2022-11-20 17:52:40 +0500

    Change color palette

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ function fetchFormData() {
   const titleInput = document.getElementById("title");
   const authorInput = document.getElementById("author");
   const pagesInput = document.getElementById("pages");
-  const readInput = document.getElementById("read");
+  const readInput = document.getElementById("check-read");
 
   return [
     titleInput.value,
